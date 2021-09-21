4450981e72bebd419ff49d6b392cac6db65011dd
Lucas Marchesin <83802271+L0ghunt@users.noreply.github.com>
2021-09-21 02:46:19 -0300

    Update and rename PortHunt.py to function.py
    [Funções ]

--- function.py
+++ function.py
@@ -3,9 +3,9 @@ import socket
 import os
 import sys
 
+
 def Logo():
     print('''
-    
  _______  _______  _______ _________                   _       _________
 (  ____ )(  ___  )(  ____ )\__   __/|\     /||\     /|( (    /|\__   __/
 | (    )|| (   ) || (    )|   ) (   | )   ( || )   ( ||  \  ( |   ) (   
@@ -15,9 +15,8 @@ def Logo():
 | )      | (___) || ) \ \__   | |   | )   ( || (___) || )  \  |   | |   
 |/       (_______)|/   \__/   )_(   |/     \|(_______)|/    )_)   )_(1.0  
                                                                         
+    ''')
 
-    '''
-    )
 
 def main():
     Sis()
@@ -25,20 +24,22 @@ def main():
     Argumentos()
     Conectar()
 
+
 def Sis():
     if sys.platform != "linux2":
-        os.system("cls")
+        os.system('cls')
     else:
-        os.system("clear")
+        os.system('clear')
+
 
 def Argumentos():
     global args
-    print("\n[+] Scanner de Portas \n")
-    print("[+] Digite PortHunt.py --help para ver os comandos\n")
+    print('\n[+] PortHunt1.0 by: L0gHunt \n')
+    print('[+] Digite PortHunt.py --help para ver os comandos\n')
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", dest="host", action="store", help="Use -s para definir o site.Ex: hys.py -s www.google.com")
-    parser.add_argument("-p", dest="port", action="store", help="Use -p para escanear uma porta expercifica Ex: -p 80")
-    parser.add_argument('-v', dest="version", action='version', version='%(prog)s 1.0 ')
+    parser.add_argument('-s', dest='host', action='store', help='Use -s para definir o alvo EXEMPLO: PortHunt.py -s www.google.com')
+    parser.add_argument('-P', dest='port', action='store', help='Use -P para definir uma porta especifica EXEMPLO: PortHunt.py -s www.google.com -P 80')
+    parser.add_argument('-V', dest='version', action='version', version='%(prog)s 1.0 ')
     args = parser.parse_args()
 
 
@@ -50,8 +51,9 @@ def Conectar():
         if c == 0:
             print("\nPorta"+args.port+"\nAberta")
         else:
-                print("\nPorta"+args.port+"\nFechada")
+            print("\nPorta"+args.port+"\nFechada")
     else:
-        quit()
+        quit
+
 
 main()

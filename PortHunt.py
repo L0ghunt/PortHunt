2b011f4222b3704993ee99b96fe9bcaefcd34dc5
Lucas Marchesin <83802271+L0ghunt@users.noreply.github.com>
2021-10-01 16:31:38 -0300

    Update PortHunt.py

--- PortHunt.py
+++ PortHunt.py
@@ -1,10 +1,13 @@
-# 
-# 
-#--------------------------------------# 
-#                                      #
-#           Em construção              #
-#                                      #
-#                                      #
-#--------------------------------------#
-#
-#
+from functions import *
+import argparse
+import socket
+import os
+import sys
+
+
+def main():
+    PortHuntlogo()
+    escolhas()
+
+main()
+
